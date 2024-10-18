bed08cff7125d2f59328032a0980387a1e6fd851
Astramentis <WesleyFLVS@gmail.com>
2024-10-18 14:43:50 -0400

    init TwoSum

---
+++ TwoSum.py
@@ -0,0 +1,22 @@
+'''
+https://www.techiedelight.com/?problem=TwoSum
+Given an unsorted integer array, find a pair with the given sum in it.
+
+• Each input can have multiple solutions. The output should match with either one of them.
+
+Input : nums[] = [8, 7, 2, 5, 3, 1], target = 10
+Output: (8, 2) or (7, 3)
+
+• The solution can return pair in any order. If no pair with the given sum exists, the solution should return an empty tuple.
+
+Input : nums[] = [5, 2, 6, 8, 1, 9], target = 12
+Output: ()
+
+'''
+
+class Solution:
+	def findPair(self, nums: List[int], target: int) -> Tuple[int]:
+		for num in nums:
+			pass#code here
+		return
+
