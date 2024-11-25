69ca6e427801b5782aa7fd22533a426f0c068d97
Astramentis <WesleyFLVS@gmail.com>
2024-11-24 22:02:52 -0500

    SortBinaryArray init

---
+++ SortBinaryArray.py
@@ -0,0 +1,17 @@
+'''
+https://www.techiedelight.com/?problem=SortBinaryArray
+Given a binary array, in-place sort it in linear time and constant space. The output should contain all zeroes, followed by all ones.
+
+Input : [1, 0, 1, 0, 1, 0, 0, 1]
+Output: [0, 0, 0, 0, 1, 1, 1, 1]
+
+Input : [1, 1]
+Output: [1, 1]
+
+'''
+
+class Solution:
+	def sortArray(self, nums: List[int]) -> None:
+		# Write your code here...
+		return
+
